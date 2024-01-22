3ee746fca98cfb0010e3e6b91de5c421778d50af
surendhar22102002 <loverboyffking143@gmail.com>
2024-01-22 03:57:24 -0500

    Google keep clone

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Header.css'
+import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined';
+
+export default function Header() {
+    return (
+        <header>
+            <h1> <LightbulbOutlinedIcon /> Google Keep</h1>
+        </header>
+    );
+}
